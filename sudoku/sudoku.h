723fb19164634d0f067632eb41dc479edf0bb2d9
aidan hopper <aidanhop1@gmail.com>
2022-10-01 11:14:20 -0700

    added character generator

--- sudoku/sudoku.h
+++ sudoku/sudoku.h
@@ -1,11 +1,15 @@
 #ifndef SUDOKU_H
 #define SUDOKU_H
 
+#include "stack.h"
+
 void PrintBoard(char *board);
 void GetPossibleNumbers(char *board, int index, int *arr);
 int ToIndex(char character);
 void PrintPossibleNumbers(int *arr);
-void GenerateMove(char *board, int **possibleNumbers);
+void GenerateMove(char *board, int **possibleNumbers, Stack *stack);
 int FindLowestEntropy(int **possibleNumbers);
+int Rand(int range);
+char GenerateNumber(int *possibleNumbers);
 
 #endif
